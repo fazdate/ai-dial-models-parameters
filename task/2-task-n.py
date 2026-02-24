0f8d5cc816b2145a9bf2aa1986a0ca1add9655d9
Daniel Fazekas <daniel_fazekas@epam.com>
2026-02-24 11:24:56 +0100

    Completed TODOs

--- task/2-task-n.py
+++ task/2-task-n.py
@@ -1,8 +1,5 @@
 from task.app.main import run
 
-# TODO:
-#  Try the `n` parameter with different models (`deployment_name`). With the parameter `n`, we can configure how many
-#       chat completion choices to generate for each input message
 #  User massage: Why is the snow white?
 
 # Models to try:
@@ -11,9 +8,8 @@ from task.app.main import run
 # - gemini-2.5-pro
 
 run(
-    # TODO:
-    #  1. Provide `deployment_name` with model from the list above👆
-    #  2. Use `n` parameter with value in range from 1 to 5!
+    deployment_name='gpt-4o',
+    n=3
 )
 
 # Pay attention to the number of choices in the response!
